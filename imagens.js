712717d6cb592d96720811b6bbfd1ecbef66c990
Patrícia Maria de Souza <patyfil@gmail.com>
2022-10-09 02:44:13 -0300

    add arquivos

---
+++ imagens.js
@@ -0,0 +1,29 @@
+// Imagens e sons do Jogo
+
+let imagemDaEstrada;
+let imagemDoAtor;
+let imagemCarro;
+let imagemCarro2;
+let imagemCarro3;
+let somDaTrilha;
+let somDaColisao;
+let somDoPonto;
+
+function preload() {
+  imagemDaEstrada = loadImage("img/estrada.png");
+  imagemDoAtor = loadImage("img/ator.png");
+  imagemCarro = loadImage("img/carro-1.png");
+  imagemCarro2 = loadImage("img/carro-2.png");
+  imagemCarro3 = loadImage("img/carro-3.png");
+  imagemCarros = [
+    imagemCarro,
+    imagemCarro2,
+    imagemCarro3,
+    imagemCarro,
+    imagemCarro2,
+    imagemCarro3,
+  ];
+  somDaTrilha = loadSound("sons/trilha.mp3");
+  somDaColisao = loadSound("sons/colidiu.mp3");
+  somDoPonto = loadSound("sons/pontos.wav");
+}
---
+++ sketch.js
@@ -0,0 +1,25 @@
+function setup() {
+  createCanvas(500, 400);
+  somDaTrilha.loop();
+}
+
+function draw() {
+  background(imagemDaEstrada);
+  mostraAtor();
+  mostraCarro();
+  movimentaCarro();
+  movimentaAtor();
+  voltaPosicaoInicialDoCarro();
+  verificaColisao();
+  incluiPontos();
+  marcaPonto();
+}
+
+function mostraCarro(){
+    for(let i = 0; i < imagemCarros.length; i = i + 1){
+  image(imagemCarros[i], xCarros[i], yCarros[i], 50, 40);
+    print("O valor do i nesta repetição é " + i)
+    }
+}
+
+
